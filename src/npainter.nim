52448fb85c4b9f2e59be1bfdc446ad12cab55b7b
Cristian Camilo Ruiz <mrgaturus@gmail.com>
2020-11-12 21:48:33 -0500

    distort: separate inverse bilinear solutions

--- src/npainter.nim
+++ src/npainter.nim
@@ -13,6 +13,7 @@ type
     buffer: string
   CaPixel = array[4, uint8]
   CaRGBA8 = ptr array[4, uint8]
+  CaSubPixel {.used.} = array[4, uint]
 
 proc load(file: string): CaImage =
   let image = loadPNG32(file)
@@ -30,8 +31,8 @@ proc `[]`(img: CaImage, x, y: int): CaPixel =
 
 proc nearest(img: CaImage, u, v: float): CaPixel =
   let 
-    pos_u = u * float(img.w)
-    pos_v = v * float(img.h)
+    pos_u = u * float(img.w - 1)
+    pos_v = v * float(img.h - 1)
     # Pixel Sampling
   var
     x1 = floor(pos_u).int
@@ -48,8 +49,8 @@ proc nearest(img: CaImage, u, v: float): CaPixel =
 
 proc bilinear(img: CaImage, u, v: float): CaPixel =
   let
-    pos_u = u * float(img.w) - 0.5
-    pos_v = v * float(img.h) - 0.5
+    pos_u = u * float(img.w - 1)
+    pos_v = v * float(img.h - 1)
     x1 = floor(pos_u)
     y1 = floor(pos_v)
     # Interpolator
@@ -113,6 +114,8 @@ type
 
 {.compile: "painter/distort.c".}
 proc both_distort(q: var CaQuad, p: CaVector, uv: var CaVector, t: float): int32 {.importc.}
+proc both_positive(q: var CaQuad, p: CaVector, uv: var CaVector, t: float): int32 {.importc.}
+proc both_negative(q: var CaQuad, p: CaVector, uv: var CaVector, t: float): int32 {.importc.}
 proc checkboard(uv: var CaVector): uint8 {.importc, used.}
 proc perspective_check(q: var CaQuad): int32 {.importc.}
 
@@ -121,8 +124,10 @@ proc xy(v: var CaVector, x, y: float) {.inline.} =
 
 method draw(self: GUICanvas, ctx: ptr CTXRender) =
   ctx.color(uint32 0xFFFFFFFF)
+  #ctx.color(uint32 0xFFFF2f2f)
   var r = rect(0, 0, 1280, 720)
   ctx.fill(r)
+  ctx.color(uint32 0xFFFFFFFF)
   ctx.texture(r, self.tex)
   ctx.color(self.color)
   for v in self.quad.v:
@@ -147,7 +152,6 @@ method draw(self: GUICanvas, ctx: ptr CTXRender) =
     point(self.quad.v[0].x, self.quad.v[0].y)
   )
   ]#
-  self.busy = false
 
 proc check(self: GUICanvas, x, y: int32): ptr CaVector =
   let 
@@ -158,38 +162,72 @@ proc check(self: GUICanvas, x, y: int32): ptr CaVector =
       echo "reached"
       result = addr v
 
-method event(self: GUICanvas, state: ptr GUIState) =
-  if state.kind == evMouseClick:
-    self.grab = check(self, state.mx, state.my)
-  elif self.test(wGrab) and not self.busy:
-    zeroMem(addr self.buffer[0], 1280*720*4*sizeof(uint8))
-    if not isNil(self.grab):
-      self.grab.x = state.mx.float32
-      self.grab.y = state.my.float32
-    else: 
-      echo "reached amout"
-      self.amout = (state.mx.float / 512).clamp(0, 0.85)
-    var p, uv: CaVector
-    if perspective_check(self.quad) == 1:
-      for y in 0..<720:
-        for x in 0..<1280:
+proc cb_distort(g: pointer, w: ptr GUITarget) =
+  let self = cast[GUICanvas](w[])
+  var p, uv: CaVector
+  var pixel: CaPixel
+  let orient = perspective_check(self.quad)
+  if orient > 0:
+    let distort =
+      if orient == 1: both_negative
+      else: both_positive
+    for y in 0..<720:
+      for x in 0..<1280:
+        when defined(subpixel):
+          var sp: CaVector
+          var subpixel: CaSubPixel
+          for sx in 0..<8:
+            let dsx = (sx.float + 0.5) * 0.125 - 0.5
+            for sy in 0..<8:
+              let dsy = (sy.float + 0.5) * 0.125 - 0.5
+              sp.xy(x.float + dsx, y.float + dsy)
+              if distort(self.quad, sp, uv, self.amout) == 1:
+                pixel = bilinear(self.image, uv.x, uv.y)
+                subpixel[0] += pixel[0]
+                subpixel[1] += pixel[1]
+                subpixel[2] += pixel[2]
+                subpixel[3] += pixel[3]
+          # Compute Pixel Average
+          subpixel[0] = subpixel[0] shr 6
+          subpixel[1] = subpixel[1] shr 6
+          subpixel[2] = subpixel[2] shr 6
+          subpixel[3] = subpixel[3] shr 6
+            # Replace Pixel
+          self.buffer[(y * 1280 + x) * 4] = subpixel[0].uint8
+          self.buffer[(y * 1280 + x) * 4 + 1] = subpixel[1].uint8
+          self.buffer[(y * 1280 + x) * 4 + 2] = subpixel[2].uint8
+          self.buffer[(y * 1280 + x) * 4 + 3] = subpixel[3].uint8
+        else:
           p.xy(x.float, y.float)
-          if both_distort(self.quad, p, uv, self.amout) == 1:
-            let pixel = bilinear(self.image, uv.x, uv.y)
-            if x == 2 and y == 2:
-              echo uv.repr
+          if distort(self.quad, p, uv, self.amout) == 1:
+            pixel = bilinear(self.image, uv.x, uv.y)
             self.buffer[(y * 1280 + x) * 4] = pixel[0]
             self.buffer[(y * 1280 + x) * 4 + 1] = pixel[1]
             self.buffer[(y * 1280 + x) * 4 + 2] = pixel[2]
             self.buffer[(y * 1280 + x) * 4 + 3] = pixel[3]
-      self.color = uint32 0xFF2B2B2B
-    else: self.color = uint32 0xFF0000FF
-    glBindTexture(GL_TEXTURE_2D, self.tex)
-    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 1280, 720, 
-      GL_RGBA, GL_UNSIGNED_BYTE, addr self.buffer[0])
-    #glGenerateMipmap(GL_TEXTURE_2D)
-    glBindTexture(GL_TEXTURE_2D, 0)
-    self.busy = true
+    self.color = uint32 0xFF2B2B2B
+  else: self.color = uint32 0xFF0000FF
+  glBindTexture(GL_TEXTURE_2D, self.tex)
+  glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 1280, 720, 
+    GL_RGBA, GL_UNSIGNED_BYTE, addr self.buffer[0])
+  #glGenerateMipmap(GL_TEXTURE_2D)
+  glBindTexture(GL_TEXTURE_2D, 0)
+  self.busy = false
+
+method event(self: GUICanvas, state: ptr GUIState) =
+  if state.kind == evMouseClick:
+    self.grab = check(self, state.mx, state.my)
+  elif self.test(wGrab):
+    zeroMem(addr self.buffer[0], 1280*720*4*sizeof(uint8))
+    if not isNil(self.grab):
+      self.grab.x = state.mx.float32
+      self.grab.y = state.my.float32
+    else:
+      self.amout = (state.mx.float / 512).clamp(0.05, 0.75)
+    if not self.busy:
+      var target = self.target
+      pushCallback(cb_distort, target)
+      self.busy = true
 
 when isMainModule:
   var win = # -- Create GUIWindow
@@ -204,10 +242,10 @@ when isMainModule:
   block:
     var 
       quad: CaQuad
-    quad.v[0].xy(0.5, 0.5)
-    quad.v[1].xy(float(img.w) + 0.5, 0.5)
-    quad.v[2].xy(float(img.w) + 0.5, float(img.h) + 0.5)
-    quad.v[3].xy(0.5, float(img.h) + 0.5)
+    quad.v[0].xy(1.0, 1.0)
+    quad.v[1].xy(float(img.w), 1.0)
+    quad.v[2].xy(float(img.w), float(img.h))
+    quad.v[3].xy(1.0, float(img.h))
     root.quad = quad
     root.color = uint32 0xFF2B2B2B
   # -- Draw Texture
--- src/painter/distort.c
+++ src/painter/distort.c
@@ -5,6 +5,8 @@
 // TODO: Semi-SIMD Interval Check
 #include <math.h>
 
+#define MIN_UV -0.0005
+#define MAX_UV  1.0005
 // -----------------------
 // TODO: Move to algebra.c
 // -----------------------
@@ -48,22 +50,23 @@ int perspective_check(quad_t* q) {
     u = round(u * 100) * 0.01;
     v = round(v * 100) * 0.01;
 
-    // Check if Transform is Valid
-    return u > 0.0 && u < 1.0 && v > 0.0 && v < 1.0;
+    // Check if Transform is Valid and Return Orientation
+    if (u > 0.0 && u < 1.0 && v > 0.0 && v < 1.0)
+      return (cross < 0.0) + 1;
   }
   
   // Invalid Transform
   return 0;
 }
 
-void perspective_distort(quad_t* q, vec2_t p, vec2_t* uv) {
+int perspective_distort(quad_t* q, vec2_t p, vec2_t* uv) {
   vec2_t d1, d2, s;
   d1 = vec2_sub(q->v[1], q->v[2]);
   d2 = vec2_sub(q->v[3], q->v[2]);
   s = vec2_add(
     vec2_sub(q->v[0], q->v[1]),
     vec2_sub(q->v[2], q->v[3])
-  );
+  ); 
 
   // Homography Coeffients
   double det, g, h, a, b, c, d, e, f;
@@ -99,15 +102,20 @@ void perspective_distort(quad_t* q, vec2_t p, vec2_t* uv) {
   u = (A * p.x + B * p.y + C) / denom;
   v = (D * p.x + E * p.y + F) / denom;
 
-  // Check Inside Later
-  uv->x = u; uv->y = v;
+  // Check if is inside UV Quad
+  if (u >= MIN_UV && u <= MAX_UV && v >= MIN_UV && v <= MAX_UV) {
+    uv->x = u; uv->y = v; return 1;
+  }
+
+  // Not Inside
+  return 0;
 }
 
 // -------------------------
 // Convex Bilinear Transform
 // -------------------------
 
-void bilinear_distort(quad_t* q, vec2_t p, vec2_t* uv) {
+int bilinear_distort(quad_t* q, vec2_t p, vec2_t* uv) {
   vec2_t e, f, g, h;
   // TODO: Precalculate Some
   e = vec2_sub(q->v[1], q->v[0]);
@@ -124,30 +132,53 @@ void bilinear_distort(quad_t* q, vec2_t p, vec2_t* uv) {
   k0 = vec2_cross(h, e);
 
   double v, u, d;
-  if (k2 == 0.0)
+  if (k2 == 0.0) {
     v = -k0 / k1;
-  else {
+
+    if (fabs(d = e.x + g.x * v) > 0.01)
+      u = (h.x - f.x * v) / d;
+    else {
+      d = (e.y + g.y * v);
+      u = (h.y - f.y * v) / d;
+    }
+  } else {
     double w = k1 * k1 - 4.0 * k0 * k2;
-    if (w < 0.0) return;
+    if (w < 0.0) return 0;
 
     w = sqrt(w);
     // Calculate Negative Solution
     v = (-k1 - w) / (2.0 * k2);
-    // If Outside, Positive solution
-    if (v < 0.0 || v > 1.0)
+    d = e.x + g.x * v; if (d != 0.0)
+      u = (h.x - f.x * v) / d;
+    else {
+      d = (e.y + g.y * v);
+      u = (h.y - f.y * v) / d;
+    }
+
+    // If outside, calculate positive solution, otherwise return
+    if (u < MIN_UV || u > MAX_UV || v < MIN_UV || v > MAX_UV) {
+      //printf("second solution detected %f %f\n", u, v);
       v = (-k1 + w) / (2.0 * k2);
+      d = e.x + g.x * v; if (d != 0.0)
+        u = (h.x - f.x * v) / d;
+      else {
+        d = (e.y + g.y * v);
+        u = (h.y - f.y * v) / d;
+      }
+    } else {
+      uv->x = u;
+      uv->y = v;
+      return 1;
+    }
   }
 
-  d = (e.x + g.x * v);
-  if (d != 0.0)
-    u = (h.x - f.x * v) / d;
-  else {
-    d = (e.y + g.y * v);
-    u = (h.y - f.y * v) / d;
+  // Check if is inside UV Quad
+  if (u >= MIN_UV && u <= MAX_UV && v >= MIN_UV && v <= MAX_UV) {
+    uv->x = u; uv->y = v; return 1;
   }
 
-  // Check Inside Later
-  uv->x = u; uv->y = v;
+  // Not Inside
+  return 0;
 }
 
 // -------------------------------------
@@ -157,20 +188,150 @@ void bilinear_distort(quad_t* q, vec2_t p, vec2_t* uv) {
 int both_distort(quad_t* q, vec2_t p, vec2_t* uv, double t) {
   vec2_t uv_p, uv_b;
 
-  // Calculate Perspective
-  perspective_distort(q, p, &uv_p);
-  if (uv_p.x < -0.005 || uv_p.x > 1.005 || uv_p.y < -0.005 || uv_p.y > 1.005)
-    return 0; // Not Inside Perspective
+  // Calculate and Check Perspective && Bilinear Transforms
+  if (perspective_distort(q, p, &uv_p) && bilinear_distort(q, p, &uv_b)) {
+  //  // Interpolate Both Transforms
+    uv->x = uv_b.x + t * (uv_p.x - uv_b.x);
+    uv->y = uv_b.y + t * (uv_p.y - uv_b.y);
+    // Inside UV Quad
+    return 1;
+  }
+  
+  //return bilinear_distort(q, p, uv);
+}
 
-  // Calculate Inverse Bilinear
-  bilinear_distort(q, p, &uv_b);
-  if (uv_b.x < -0.005 || uv_b.x > 1.005 || uv_b.y < -0.005 || uv_b.y > 1.005)
-    return 0; // Not Inside Bilinear
+// --------------------
+// Experimental Distort
+// --------------------
 
-  // Interpolate Both Transforms
-  uv->x = uv_b.x + t * (uv_p.x - uv_b.x);
-  uv->y = uv_b.y + t * (uv_p.y - uv_b.y);
+int bilinear_positive(quad_t* q, vec2_t p, vec2_t* uv) {
+  vec2_t e, f, g, h;
+  // TODO: Precalculate Some
+  e = vec2_sub(q->v[1], q->v[0]);
+  f = vec2_sub(q->v[3], q->v[0]);
+  g = vec2_add(
+    vec2_sub(q->v[0], q->v[1]),
+    vec2_sub(q->v[2], q->v[3])
+  );
+  h = vec2_sub(p, q->v[0]);
 
-  // Inside
-  return 1;
+  double k0, k1, k2;
+  k2 = vec2_cross(g, f);
+  k1 = vec2_cross(e, f) + vec2_cross(h, g);
+  k0 = vec2_cross(h, e);
+
+  double v, u, d;
+  if (k2 == 0.0) {
+    v = -k0 / k1;
+
+    if (fabs(d = e.x + g.x * v) > 0.01)
+      u = (h.x - f.x * v) / d;
+    else {
+      d = (e.y + g.y * v);
+      u = (h.y - f.y * v) / d;
+    }
+  } else {
+    double w = k1 * k1 - 4.0 * k0 * k2;
+    if (w < 0.0) return 0;
+
+    w = sqrt(w);
+    // Calculate Negative Solution
+    v = (-k1 + w) / (2.0 * k2);
+    d = e.x + g.x * v; if (d != 0.0)
+      u = (h.x - f.x * v) / d;
+    else {
+      d = (e.y + g.y * v);
+      u = (h.y - f.y * v) / d;
+    }
+  }
+
+  // Check if is inside UV Quad
+  if (u >= MIN_UV && u <= MAX_UV && v >= MIN_UV && v <= MAX_UV) {
+    uv->x = u; uv->y = v; return 1;
+  }
+
+  // Not Inside
+  return 0;
+}
+
+int bilinear_negative(quad_t* q, vec2_t p, vec2_t* uv) {
+  vec2_t e, f, g, h;
+  // TODO: Precalculate Some
+  e = vec2_sub(q->v[1], q->v[0]);
+  f = vec2_sub(q->v[3], q->v[0]);
+  g = vec2_add(
+    vec2_sub(q->v[0], q->v[1]),
+    vec2_sub(q->v[2], q->v[3])
+  );
+  h = vec2_sub(p, q->v[0]);
+
+  double k0, k1, k2;
+  k2 = vec2_cross(g, f);
+  k1 = vec2_cross(e, f) + vec2_cross(h, g);
+  k0 = vec2_cross(h, e);
+
+  double v, u, d;
+  if (k2 == 0.0) {
+    v = -k0 / k1;
+
+    if (fabs(d = e.x + g.x * v) > 0.01)
+      u = (h.x - f.x * v) / d;
+    else {
+      d = (e.y + g.y * v);
+      u = (h.y - f.y * v) / d;
+    }
+  } else {
+    double w = k1 * k1 - 4.0 * k0 * k2;
+    if (w < 0.0) return 0;
+
+    w = sqrt(w);
+    // Calculate Negative Solution
+    v = (-k1 - w) / (2.0 * k2);
+    d = e.x + g.x * v; if (d != 0.0)
+      u = (h.x - f.x * v) / d;
+    else {
+      d = (e.y + g.y * v);
+      u = (h.y - f.y * v) / d;
+    }
+  }
+
+  // Check if is inside UV Quad
+  if (u >= MIN_UV && u <= MAX_UV && v >= MIN_UV && v <= MAX_UV) {
+    uv->x = u; uv->y = v; return 1;
+  }
+
+  // Not Inside
+  return 0;
+}
+
+int both_positive(quad_t* q, vec2_t p, vec2_t* uv, double t) {
+  vec2_t uv_p, uv_b;
+
+  // Calculate and Check Perspective && Bilinear Transforms
+  if (perspective_distort(q, p, &uv_p) && bilinear_positive(q, p, &uv_b)) {
+  //  // Interpolate Both Transforms
+    uv->x = uv_b.x + t * (uv_p.x - uv_b.x);
+    uv->y = uv_b.y + t * (uv_p.y - uv_b.y);
+    // Inside UV Quad
+    return 1;
+  }
+
+  // Not Inside
+  return 0;
+}
+
+int both_negative(quad_t* q, vec2_t p, vec2_t* uv, double t) {
+  vec2_t uv_p, uv_b;
+
+  // Calculate and Check Perspective && Bilinear Transforms
+  if (perspective_distort(q, p, &uv_p) && bilinear_negative(q, p, &uv_b)) {
+  //  // Interpolate Both Transforms
+    uv->x = uv_b.x + t * (uv_p.x - uv_b.x);
+    uv->y = uv_b.y + t * (uv_p.y - uv_b.y);
+    // Inside UV Quad
+    return 1;
+  }
+
+  // Not Inside
+  return 0;
 }
